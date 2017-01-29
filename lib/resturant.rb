5c9f47c97fb12418bc2ac68e48b4fe747c82741e
rasna shakya <rasnashakya@gmail.com>
2017-01-30 01:43:06 +0545

    given interactive method to resturant file to react woth other file system

--- lib/resturant.rb
+++ lib/resturant.rb
@@ -6,10 +6,26 @@ class Resturant
 	end
 
 	def self.file_exists?
+		#class should know if the resturant file exists
+		if @@filepath && File.exists?(@@filepath)
+			return true
+		else
+			return false
+		end
+	end
 
+	def self.file_usable?
+		return false unless @@filepath
+		return false unless File.exists?(@@filepath)
+		return false unless File.readable?(@@filepath)
+		return false unless File.writable?(@@filepath)
+		return true
 	end
 
 	def self.create_file
+		#create resturant file
+		File.open(@@filepath, 'w') unless file_exists?
+		return file_usable?
 
 	end
 
