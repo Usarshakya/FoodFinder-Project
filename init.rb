c8d7135b576a9345a2a34b4cef9f2e1c77ee9d0b
rasna shakya <rasnashakya@gmail.com>
2017-01-30 00:19:18 +0545

    Initialized basic path to load in other path

---
+++ init.rb
@@ -0,0 +1,9 @@
+###Food Finder###
+
+App_root = File.dirname(__FILE__)
+
+# require "#{App_root}/lib/guide"
+# require File.join(App_root,'lib', 'guide')
+
+$:.unshift(File.join(App_root,'lib'))
+require 'guide'
