0952488fccfd8b071c3d295f49202975f8d59021
rasna shakya <rasnashakya@gmail.com>
2017-01-30 02:03:30 +0545

    handled input action

--- lib/guide.rb
+++ lib/guide.rb
@@ -2,33 +2,57 @@ require 'resturant'
 
 class Guide
 
-	def initialize(path=nil)
-		#locate the resturant text file at path
-		Resturant.filepath = path
-		if Resturant.file_exists?
-			puts "Found Resutrant file"
-		#or create a new file
-		elsif Resturant.create_file
-			puts "Created resturant file" 
-		# exit if create fails
-		else
-			puts "Exiting...\n\n"
-			exit!
-		end
-	end
+  def initialize(path=nil)
+    #locate the resturant text file at path
+    Resturant.filepath = path
+    if Resturant.file_exists?
+      puts "Found Resutrant file"
+    #or create a new file
+    elsif Resturant.create_file
+      puts "Created resturant file" 
+    # exit if create fails
+    else
+      puts "Exiting...\n\n"
+      exit!
+    end
+  end
 
-	def launch
-		introduction
-		conclusion
-	end
+  def launch
+    introduction 
+    #action loop
+    result = nil
+    until result == :quit
+      #what do you want to do? (list, add, find, quit)
+      print ">"
+      user_response=gets.chomp
+      # do thataction
+      result = do_action(user_response)
+    end
+    conclusion
+  end
 
-	def introduction
-		puts "\n\n<<<WELCOME TO THE FOODFINDER>>>\n\n"
-		puts "This is an interactive guide to help you find the food you crave.\n\n"
-	end
+  def do_action(action)
+    case action
+    when 'list'
+      puts "Listing.."
+    when 'find'
+      puts "Finding..."
+    when 'add'
+      puts "Adding..."
+    when 'quit'
+      return :quit
+    else
+      puts "\nI dont understand that command"
+    end 
+  end
 
-	def conclusion
-		puts "\n<<<GUDBYE>>>\n\n\n"
-	end
+  def introduction
+    puts "\n\n<<<WELCOME TO THE FOODFINDER>>>\n\n"
+    puts "This is an interactive guide to help you find the food you crave.\n\n"
+  end
+
+  def conclusion
+    puts "\n<<<GUDBYE>>>\n\n\n"
+  end
 
 end
